cc91d5fa2cfa6b7af0fe7103c810d696901e8213
unknown <ewq3167@gmail.com>
2020-11-04 23:38:31 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
+import React from "react"; // 이거 왜 넣으라고 했더라
 import logo from './logo.svg';
+import SearchInput from "./component/SearchInput"
+import SearchResult from "./component/SearchResult"
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <SearchInput />
+      <SearchResult />
+    </>
   );
 }
 
